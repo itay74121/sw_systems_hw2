71884f6991b1d2b6d1d0921cf4c544646a61df88
itay yosef <itay74121@gmail.com>
2020-12-05 12:06:03 +0200

    Lost some chnages since i didn't commit last time hence so im going to commit this time. HAHA...

--- main.c
+++ main.c
@@ -7,19 +7,10 @@ void menu()
     char str [] = "\nPlease choose a transaction type:\n O-Open Account\n B-Balance Inquiry\n D-Deposit\n W-Withdrawal\n C-Close Account\n I-Interest\n P-Print\n E-Exit";
     printf("%s\n",str);
 }
-void lower(char * c)
-{
-    if(*c>='A' && *c<='Z')
-    {
-        *c -=  'A';
-    }
-}
+
 int main()
 {
-    char c=0;
-    char t;
-    char * pc = &c;
-
+    char c;
     int account;
     float amount;
     int status;
@@ -30,13 +21,13 @@ int main()
         menu();
         printf("Enter transaction type: ");
         scanf("%c",&c);
-        lower(pc);
+
         switch (c)
         {
         case 0:
             printf("\nInvalid transaction type\n");
             break;
-        case 'o':
+        case 'O':
             printf("Please enter amount for deposit: ");
             status = scanf("%f%c",&amount,&t);
             if (status==0)
@@ -44,7 +35,7 @@ int main()
             if(status==2)
                 O(accounts,amount);
             break;
-        case 'b':
+        case 'B':
             printf("Please enter amount for deposit: ");
             status = scanf("%d%c",&account,&t);
             if(status == 0)
@@ -54,7 +45,7 @@ int main()
                 B(accounts, account);
             }
             break;
-        case 'd':
+        case 'D':
             printf("Please enter account number and amount: ");
             status = scanf("%d %f%c",&account,&amount,&t);
             if(status!=3)
@@ -64,7 +55,7 @@ int main()
                 D(accounts,account,amount);
             }
             break;
-        case 'w':
+        case 'W':
             printf("Please enter account number and amount: ");
             status = scanf("%d %f%c",&account,&amount,&t);
             if(status!=3)
@@ -74,7 +65,7 @@ int main()
                 W(accounts,account,amount);
             }
             break;
-        case 'c':
+        case 'C':
             printf("Please enter account number: ");
             status = scanf("%d%c",&account,&t);
             if(status == 0)
@@ -84,7 +75,7 @@ int main()
                 C(accounts, account);
             }
             break;
-        case 'i':
+        case 'I':
             printf("Please enter account number: ");
             status = scanf("%f",&amount);
             if(status==0)
@@ -94,11 +85,11 @@ int main()
                 I(accounts,amount);
             }
             break;
-        case 'p':
+        case 'P':
             P(accounts);
             scanf("%c",&t);
             break;
-        case 'e':
+        case 'E':
             E(accounts);
             c='E';
             break;   
